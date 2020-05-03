c8fef4c825ce19545db666d9f2c127d56ce039ae
Winston Wiradi <winstonwiradip@gmail.com>
2020-05-03 16:03:18 +0700

    KMS backend

--- kms_backend/settings.py
+++ kms_backend/settings.py
@@ -118,9 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
